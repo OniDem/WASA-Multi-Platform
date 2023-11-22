409314c67792501a533a757ff05d7a2903c8a2c6
OniDem1 <spaindecraft17@gmail.com>
2023-11-22 14:10:12 +0700

    Добавил класс для хранения информации о штрихкоде

---
+++ WASA-Multi-Platform/Const/BarcodeInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WASA_Multi_Platform.Const
+{
+    public static class BarcodeInfo
+    {
+        public static bool BarcodeSended {  get; set; }
+
+        public static string Barcode {  get; set; }
+    }
+}
