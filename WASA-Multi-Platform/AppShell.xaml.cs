87b5b0de07ad4115fdf70e6c13968b59043ef596
OniDem1 <spaindecraft17@gmail.com>
2023-11-23 18:04:44 +0700

    Добавил боковое меню

--- WASA-Multi-Platform/AppShell.xaml.cs
+++ WASA-Multi-Platform/AppShell.xaml.cs
@@ -1,10 +1,24 @@
-﻿namespace WASA_Multi_Platform
+﻿using WASA_Multi_Platform.Entity;
+
+namespace WASA_Multi_Platform
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
+            UserActivities.GetUserInfo(3);
+            SessionUser.Text = UserEntity.UserName;
+        }
+
+        private void SessionExit_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SessionUser_Clicked(object sender, EventArgs e)
+        {
+
         }
     }
 }
