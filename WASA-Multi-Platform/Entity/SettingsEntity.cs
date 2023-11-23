0174016772dd86f848767dd9f2cc0d6bf0e631b5
OniDem1 <spaindecraft17@gmail.com>
2023-11-24 00:01:35 +0700

    Добавил функциюнал вывода и добовления настроек

---
+++ WASA-Multi-Platform/Entity/SettingsEntity.cs
@@ -0,0 +1,15 @@
+﻿namespace WASA_Multi_Platform.Entity
+{
+    public class SettingsEntity
+    {
+        public string DB_Name { get; set; }
+
+        public string DB_Ip { get; set; }
+
+        public string DB_Port { get; set; }
+
+        public string DB_UserName { get; set; }
+
+        public string DB_Password { get; set; }
+    }
+}
--- WASA-Multi-Platform/Pages/SettingsPage.xaml.cs
+++ WASA-Multi-Platform/Pages/SettingsPage.xaml.cs
@@ -1,9 +1,36 @@
+using WASA_Multi_Platform.Activities;
+using WASA_Multi_Platform.Entity;
+
 namespace WASA_Multi_Platform.Pages;
 
 public partial class SettingsPage : ContentPage
 {
-	public SettingsPage()
+	SettingsEntity entity = new();
+
+    public SettingsPage()
 	{
 		InitializeComponent();
+		Task.Run(async () => await Dispatcher.DispatchAsync(() =>
+		{
+            entity = FileIOActivities.GetAddressData();
+        }));
+		Name.Text = entity.DB_Name;
+		IP.Text = entity.DB_Ip;
+		Port.Text = entity.DB_Port;
+		UserName.Text = entity.DB_UserName;
+		Password.Text = entity.DB_Password;
 	}
+
+    private void SaveButton_Clicked(object sender, EventArgs e)
+    {
+		FileIOActivities.SetAddressData(new()
+		{
+			DB_Name = Name.Text,
+			DB_Ip = IP.Text,
+			DB_Port = Port.Text,
+			DB_UserName = UserName.Text,
+			DB_Password = Password.Text,
+		});
+		Navigation.RemovePage(this);
+    }
 }
