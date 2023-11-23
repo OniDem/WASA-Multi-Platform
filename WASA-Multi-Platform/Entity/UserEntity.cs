fbe8b9fd473872907398a67cbdcced1483a9ba33
OniDem1 <spaindecraft17@gmail.com>
2023-11-23 18:05:17 +0700

    Сделал сущность и класс для получения информации о пользователе

---
+++ WASA-Multi-Platform/Entity/UserEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace WASA_Multi_Platform.Entity
+{
+    static class UserEntity
+    {
+        public static int ID { get; set; }
+
+        public static string UserName { get; set; }
+
+        public static string UserRole { get; set; }
+    }
+}
